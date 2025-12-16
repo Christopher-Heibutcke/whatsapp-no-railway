29ddacc2143a7c782f4eb73af505e6c95c42d3d3
Christopher Heibutcke <63764602+Christopher-Heibutcke@users.noreply.github.com>
2025-12-15 23:39:22 -0300

    Add webVersionCache configuration to server.js

--- server.js
+++ server.js
@@ -113,6 +113,10 @@ function initializeWhatsApp() {
       dataPath: "./whatsapp_sessions",
       clientId: "eyescloud-main",
     }),
+    webVersionCache: {
+      type: "remote",
+      remotePath: "https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html",
+    },
     puppeteer: {
       headless: true,
       executablePath: chromiumPath,
